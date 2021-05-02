222d89444c47fff3b6daf15a6617aee15a4d8dff
nghia3110 <77100524+nghia3110@users.noreply.github.com>
2021-05-02 20:49:15 +0700

    Add files via upload

---
+++ code/main.cpp
@@ -0,0 +1,47 @@
+﻿#include "Texture.h"
+#include "Buttons.h"
+#include "Variables.h"
+#include "constant.h"
+#include "function.h"
+using namespace std;
+
+
+int WinMain(int argc, char* argv[])
+{
+	//Start up SDL and create window
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		if (loadmedia())
+		{
+			if (loadMenuMedia())
+			{
+				showMenu();
+				while (mainLoop)
+				{
+					if (isChoosing)
+					{
+						showModeChoice();
+					}
+					if(customMode) CustomMode();
+					while (isRunning)
+					{
+						handleEvent();
+						setButtonPosition();
+						renderGame();
+					}
+				}	
+			}
+		}
+	}
+	//Free resources and close SDL
+	close();
+	return 0;
+}
+
+
+
+
